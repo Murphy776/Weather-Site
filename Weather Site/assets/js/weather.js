52ca2001292f5dee49794df3d32a9cf22891947d
Kevin Murphy <KLMurphy677@gmail.com>
2018-03-11 16:37:49 -0500

    Add files via upload

--- Weather Site/assets/js/weather.js
+++ Weather Site/assets/js/weather.js
@@ -3,30 +3,52 @@ var apiCall = "http://api.openweathermap.org/data/2.5/weather?";
 var userInput = "60506";
 var input = "zip=" + userInput + "&units=imperial";
 var apiKey = "&APPID=5029ddb0183e8082011dff2ad91e21cb";
-
+var zipCode = " ";
 
 $(document).ready(function(){
-		var city = $('#city').val();
+	
+	if(zipCode == " "){
+		zipCode = "60506";
+	}
 
-		
-			$.ajax({
-				url: "http://api.openweathermap.org/data/2.5/weather?zip=60506&units=imperial&APPID=5029ddb0183e8082011dff2ad91e21cb",
+	$.ajax({
+		url: "http://api.openweathermap.org/data/2.5/weather?zip="+ zipCode +"&units=imperial&APPID=5029ddb0183e8082011dff2ad91e21cb",
+		type: "GET",
+		dataType: "jsonp",
+		success: function(data){
+			var name = data.name;
+			var temp = Math.round(data.main.temp);
+			var desc = data.weather[0].description;
+			var iconCode = data.weather[0].icon;
+			$("#temp").html("<h1>" + name +"</h1><h3>" + temp + "<br/>" + desc + "</h3>");
+			$("#icon").html("<img src='http://openweathermap.org/img/w/" + iconCode + ".png'>");
+		}
+
+	});
+
+
+
+});
+
+function getUserInput(){
+				var zipCode = $('#zip').val();
+				console.log(zipCode);
+				$.ajax({
+				url: "http://api.openweathermap.org/data/2.5/weather?zip="+ zipCode +"&units=imperial&APPID=5029ddb0183e8082011dff2ad91e21cb",
 				type: "GET",
 				dataType: "jsonp",
 				success: function(data){
 					var name = data.name;
-					var temp = data.main.temp;
+					var temp = Math.round(data.main.temp);
 					var desc = data.weather[0].description;
 					var iconCode = data.weather[0].icon;
 
-					$("#temp").html("<h1>" + name +"</h1>" + temp + "<br/>" + desc);
+					$("#temp").html("<h1>" + name +"</h1><h3>" + temp + "<br/>" + desc + "</h3>");
 
 					$("#icon").html("<img src='http://openweathermap.org/img/w/" + iconCode + ".png'>");
 
 				}
 
 
-			});
-
-
-});
+				});
+			}
